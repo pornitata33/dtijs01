f00b70cc6e6258227938008f9650f416f55b3549
pornitata33 <pornitata33@gmail.com>
2026-09-08 16:53:13 +0700

    JS01-10

---
+++ myjs08.js
@@ -0,0 +1,16 @@
+//
+
+function funcA(a, b, c,){
+    console.log(a, b. c)
+}
+
+
+function funcB(a, b= 555, c= 999,){
+    console.log(a, b. c)
+}
+
+funcA(10,20,30)
+funcA(111)
+console.log('++++++++++++++++++++')
+funcB(10,20,30)
+funcB(111)
